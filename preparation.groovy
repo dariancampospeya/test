2e635571e6b0e246784b524802a2dee966d52d6b
Darian Campos <darian.campos@pedidosya.com>
2020-03-13 15:52:28 -0300

    update

---
+++ preparation.groovy
@@ -0,0 +1,35 @@
+pipeline {
+    agent { label "master"}
+   
+    //add environment
+    environment {
+       GITHUBREPO = ''
+    }
+
+    stages {
+        //inject stage code
+        stage('Preparation') {
+                    steps {
+                       sh 'curl -OL https://raw.githubusercontent.com/dariancampospeya/test/master/extravars.properties'
+            
+                            script {
+                                def props = readProperties file: 'extravars.properties'
+                                env.nombre = props.nombre
+                                env.apellido = props.apellido
+                                env.edad = props.edad
+                            }
+            
+                        sh "echo My name is $nombre"
+                        sh "echo My lastname is $apellido"
+                        sh "echo My year is $edad"  
+            }
+        }
+        //use your code stage pipeline
+        stage('develop'){
+            steps{
+                sh "echo write your code pipeline"
+            }
+        }
+    }
+
+}
