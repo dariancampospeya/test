8ca011f239f67a692b40308fcd09952874c6247a
Darian Campos <darian.campos@pedidosya.com>
2020-03-17 09:50:20 -0300

    update

--- preparation.groovy
+++ preparation.groovy
@@ -8,22 +8,7 @@ pipeline {
 
     stages {
         //inject stage code
-        stage('Preparation') {
-                    steps {
-                       sh 'curl -OL https://raw.githubusercontent.com/dariancampospeya/test/master/extravars.properties'
-            
-                            script {
-                                def props = readProperties file: 'extravars.properties'
-                                env.nombre = props.nombre
-                                env.apellido = props.apellido
-                                env.edad = props.edad
-                            }
-            
-                        sh "echo My name is $nombre"
-                        sh "echo My lastname is $apellido"
-                        sh "echo My year is $edad"  
-            }
-        }
+        {% include 'https://raw.githubusercontent.com/dariancampospeya/test/master/_preparation.groovy' with context %}
 
         //use your code stage pipeline
         stage('develop'){
