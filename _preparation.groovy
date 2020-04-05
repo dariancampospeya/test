190a0c99beaa2b5bb32fd7968043b3b6243c5a25
Darian Campos <58445556+dariancampospeya@users.noreply.github.com>
2020-04-05 20:48:55 -0300

    update

--- _preparation.groovy
+++ _preparation.groovy
@@ -1,16 +1,3 @@
-stage('Preparation') {
-            steps {
-               sh 'curl -OL https://raw.githubusercontent.com/dariancampospeya/test/master/extravars.properties'
-    
-                    script {
-                        def props = readProperties file: 'extravars.properties'
-                        env.nombre = props.nombre
-                        env.apellido = props.apellido
-                        env.edad = props.edad
-                    }
-    
-                sh "echo My name is $nombre"
-                sh "echo My lastname is $apellido"
-                sh "echo My year is $edad"  
-            }
-        }
+steps {
+            sh "echo Aca está el codigo del dev"
+}
