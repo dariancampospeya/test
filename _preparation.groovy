3cf1b67dcb951b0ce3140aa6547315d299b1bb53
Darian Campos <58445556+dariancampospeya@users.noreply.github.com>
2020-04-05 21:53:11 -0300

    test

--- _preparation.groovy
+++ _preparation.groovy
@@ -1,10 +1,5 @@
 def includeCode() {
-  stage('dev') {
-            steps {
-                        sh "echo Aca está el codigo del dev"
-            }
-  }
-
+  sh "echo Aca está el codigo del dev"
 }
 
 return this;
